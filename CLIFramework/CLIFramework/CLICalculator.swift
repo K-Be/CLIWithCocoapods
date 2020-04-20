df13064a22ac27998bc358353734b5160aa20984
Andrew Romanov <scalli-k-be@ya.ru>
2020-04-20 14:38:37 +0700

    restored CLIFramework

---
+++ CLIFramework/CLIFramework/CLICalculator.swift
@@ -0,0 +1,23 @@
+//
+//  CLICalculator.swift
+//  CLIFramework
+//
+//  Created by Andrew Romanov on 20.04.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+import Foundation
+import SwiftyTimer
+
+public struct Calculator {
+	
+	public init() {
+	}
+	
+	public func sum(_ val1 : Int, _ val2 : Int) -> Int {
+		
+		return val1 + val2
+	}
+	
+}
+
---
+++ CLIFramework/CLIFramework/CLIFramework.h
@@ -0,0 +1,19 @@
+//
+//  CLIFramework.h
+//  CLIFramework
+//
+//  Created by Andrew Romanov on 20.04.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLIFramework.
+FOUNDATION_EXPORT double CLIFrameworkVersionNumber;
+
+//! Project version string for CLIFramework.
+FOUNDATION_EXPORT const unsigned char CLIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLIFramework/PublicHeader.h>
+
+
